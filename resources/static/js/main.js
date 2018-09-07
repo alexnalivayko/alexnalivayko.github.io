457cb8f25dd36d7cf15da96917105ff6798d4554
Aleksandr Nalivaiko <aleksandr.nalivaiko@netcracker.com>
2018-09-07 16:42:05 +0400

    Initial commit

---
+++ resources/static/js/main.js
@@ -0,0 +1,32 @@
+jQuery(document).ready(function($) {
+
+    /*======= Skillset *=======*/
+    
+    $('.level-bar-inner').css('width', '0');
+    
+    $(window).on('load', function() {
+
+        $('.level-bar-inner').each(function() {
+        
+            var itemWidth = $(this).data('level');
+            
+            $(this).animate({
+                width: itemWidth
+            }, 800);
+            
+        });
+
+    });
+    
+    /* Bootstrap Tooltip for Skillset */
+    $('.level-label').tooltip();
+
+
+    /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
+    GitHubCalendar("#", "");
+    
+    
+    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
+    GitHubActivity.feed({ username: "", selector: "#" });
+
+});
